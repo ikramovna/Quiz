eb22003fcf2c3e33ec32bfdba2708e7e0b6edfc9
spiker <abdulboriy1004@gmail.com>
2023-10-30 17:32:44 +0500

    first commit

---
+++ root/urls.py
@@ -0,0 +1,22 @@
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path, include
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
+from root.swagger import schema_view
+
+urlpatterns = [
+    path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+
+    path('admin/', admin.site.urls),
+
+    path('token', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/v1/users/', include('users.urls')),
+]
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
---
+++ users/serializers.py
@@ -0,0 +1,68 @@
+import random
+
+from django.contrib.auth.hashers import make_password
+from django.core.mail import send_mail
+from rest_framework import serializers
+
+from root.settings import EMAIL_HOST_USER
+from users.models import User, getKey, setKey
+
+
+class UserRegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(max_length=150, write_only=True)
+
+    class Meta:
+        model = User
+        fields = ("full_name", "email", "username", "password")
+
+    def validate(self, attrs):
+        activate_code = random.randint(100000, 999999)
+        user = User(
+            full_name=attrs['full_name'],
+            email=attrs['email'],
+            username=attrs['username'],
+            password=make_password(attrs['password']),
+            is_active=True,
+        )
+        setKey(
+            key=attrs['email'],
+            value={
+                "user": user,
+                "activate_code": activate_code
+            },
+            timeout=300
+        )
+        print(getKey(key=attrs['email']))
+        send_mail(
+            subject="Subject here",
+            message=f"Your activate code.\n{activate_code}",
+            from_email=EMAIL_HOST_USER,
+            recipient_list=[attrs['email']],
+            fail_silently=False,
+        )
+        return super().validate(attrs)
+
+
+class CheckActivationCodeSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+    activate_code = serializers.IntegerField(write_only=True)
+
+    def validate(self, attrs):
+        data = getKey(key=attrs['email'])
+        print(data)
+        if data and data['activate_code'] == attrs['activate_code']:
+            return attrs
+        raise serializers.ValidationError(
+            {"error": "Error activate code or email"}
+        )
+
+
+class ResetPasswordSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+
+
+class ResetPasswordConfirmSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+    activation_code = serializers.CharField()
+    new_password = serializers.CharField()
+    confirm_password = serializers.CharField()
