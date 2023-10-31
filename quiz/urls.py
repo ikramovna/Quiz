cdc4535ce6619917ed8a4b39837191b62c67750c
muslima <muslimazokirjonova2004@gmail.com>
2023-10-31 13:28:09 +0500

    Initial commit

--- quiz/urls.py
+++ quiz/urls.py
@@ -3,9 +3,9 @@ from django.urls import path
 from .views import CategoryListView, PostUserAnswerApiView, CategoryDetailView, SendMailAPIView
 
 urlpatterns = [
-    path('api/v1/category', CategoryListView.as_view()),
-    path('api/v1/category/<int:pk>', CategoryDetailView.as_view()),
-    path('api/v1/answer', PostUserAnswerApiView.as_view()),
+    path('category', CategoryListView.as_view()),
+    path('category/<int:pk>', CategoryDetailView.as_view()),
+    path('answer', PostUserAnswerApiView.as_view()),
 
-    path('api/v1/send_mail', SendMailAPIView.as_view()),
+    path('send_mail', SendMailAPIView.as_view()),
 ]
