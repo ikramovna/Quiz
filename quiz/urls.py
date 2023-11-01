b6dca2051c61409300ec0e4ed820805a9cd5c9e3
Muslima Zakirjanova <122157752+ikramovna@users.noreply.github.com>
2023-11-01 23:18:48 +0500

    Update urls.py

--- quiz/urls.py
+++ quiz/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 
-from .views import CategoryListView, PostUserAnswerApiView, CategoryDetailView, SendMailAPIView
+from .views import CategoryListView, PostUserAnswerApiView, CategoryDetailView, SendMailAPIView, UserAnswersListView
 
 urlpatterns = [
     path('category', CategoryListView.as_view()),
     path('category/<int:pk>', CategoryDetailView.as_view()),
     path('answer', PostUserAnswerApiView.as_view()),
 
+    
+     path('user/answer', UserAnswersListView.as_view()),
     path('send_mail', SendMailAPIView.as_view()),
 ]
